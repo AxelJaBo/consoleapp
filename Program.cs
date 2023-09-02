f850e4d3217d8491b1778e7ff4cbdebe7c2e49d3
AxelJaBo <axel.910921@gmail.com>
2023-09-01 17:35:23 -0700

    compilación y ejecución

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Por favor ingrese un nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su edad");
+var edad = Console.ReadLine();
+
+Console.WriteLine($"Mi nombre es {nombre}, trabajo como {cargo} y tengo {edad} años.");
